6178e2548ff60ad64d96924f18c9bea825dbffad
Dongzt <79237845+Dongzt@users.noreply.github.com>
2022-08-29 21:33:55 +0800

    Add files via upload

---
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -0,0 +1,39 @@
+package cn.ofpp;
+
+import cn.hutool.core.util.StrUtil;
+import cn.ofpp.core.Wx;
+
+/**
+ * 引导配置
+ *
+ * @author DokiYolo
+ * Date 2022-08-22
+ */
+public class Bootstrap {
+
+    /**
+     * 公众号AppID
+     */
+    public static final String APP_ID = "wxe8ece1b43af3f968";
+
+    /**
+     * 公众号秘钥
+     */
+    public static final String SECRET = "c5e59f7724f32fa20666be43d844cdb4";
+
+    /**
+     * 全局模板ID  也可针对单个Friend指定模板
+     */
+    public static final String TEMPLATE_ID = "o6_aZ6JYQTHK5S66_trJMiheQFhY";
+
+    /**
+     * 初始化
+     */
+    public static void init() {
+        if (StrUtil.hasEmpty(APP_ID, SECRET, TEMPLATE_ID)) {
+            throw new IllegalArgumentException("请检查配置");
+        }
+        Wx.init();
+    }
+
+}
